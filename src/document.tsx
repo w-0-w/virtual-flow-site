15f2a9d15ff01a1844c16014047a8fce231a0ddf
realign <realign_tech@163.com>
2025-05-03 22:09:00 +0800

    update

--- src/document.tsx
+++ src/document.tsx
@@ -41,6 +41,14 @@ export default function Document({ pagePath }) {
               "window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'AW-17046827907');",
           }}
         />
+        {/* <!-- Twitter conversion tracking base code --> */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html:
+              "!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);},s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');twq('config','po3b5');",
+          }}
+        />
+        {/* <!-- End Twitter conversion tracking base code --> */}
       </head>
       <body>
         <Main />
